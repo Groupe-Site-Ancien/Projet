15ad5fbce3b8787ae64b5f61a48db43fe6c2e777
MelodyJoos <melody.joos@gmail.com>
2020-04-08 17:59:27 +0200

    Refonte design de la page d'accueil espace membre

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -1,3 +1,10 @@
+<!-- style des balises et classes -->
+<style type="text/css">
+    .titre {
+        font-weight: bold;
+    }
+</style>
+
 @extends('layouts.app')
 
 @section('content')
@@ -8,9 +15,10 @@
                     <div class="card-header">Mon tableau de bord</div>
 
                         <div style="padding: 20px;">
+                            <p style="font-size: 12pt;">
                             Bonjour {{$user->profile->prenom ?? 'Prénom' }} !
                             <br>
-                            Vous êtes connecté avec l'identifiant : {{ $user->id }}
+                                Bienvenue sur votre espace personnel des <span style="font-weight: bolder">Anciens du Master CCI</span> :)
                             <br>
                             Nom : {{$user->profile->nom ?? 'Nom'}}
                             <br>
@@ -18,6 +26,7 @@
                             @can('update', $user->profile)
                             <a href="/profiles/{{$user->id}}/edit">Modifier mon profil</a>
                             @endcan
+                            </p>
                         </div>
                 </div>
             </div>
@@ -29,7 +38,7 @@
         <div class="card col-2 offset-2">
             <img class="card-img-top" src="/jpg/temoignage.jpg" alt="Témoignage">
             <div class="card-body">
-                <h5 class="card-title">Témoignage</h5>
+                <h5 class="card-title titre">Témoignage</h5>
                 <p class="card-text">Je raconte comment s'est passé le Master CCI et ce que cela m'a apporté.</p>
                 <a href="#" class="btn btn-primary">Je témoigne !</a>
             </div>
@@ -37,7 +46,7 @@
         <div class="card col-2 offset-1">
             <img class="card-img-top" src="/jpg/enquete.jpg" alt="Enquête">
             <div class="card-body">
-                <h5 class="card-title">Enquête</h5>
+                <h5 class="card-title titre">Enquête</h5>
                 <p class="card-text">Je réponds à l'enquête sur mon parcours après le Master CCI.</p>
                 <a href="#" class="btn btn-primary">Je réponds !</a>
             </div>
@@ -45,7 +54,7 @@
         <div class="card col-2 offset-1">
             <img class="card-img-top" src="/png/stage.png" alt="Stage">
             <div class="card-body">
-                <h5 class="card-title">Stage</h5>
+                <h5 class="card-title titre">Stage</h5>
                 <p class="card-text">Mon entreprise ou des connaissances cherchent des stagiaires ?</p>
                 <a href="#" class="btn btn-primary">Je dépose une offre !</a>
             </div>
